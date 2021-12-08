bfb546dcaf42ce1c47c03e06e63302690e093ed8
Karl <local-part@domain>
2021-12-08 03:44:00 +0100

    [/pages/] index.vue updated.

--- pages/index.vue
+++ pages/index.vue
@@ -345,10 +345,15 @@ body,
 </style>
 <style>
 @font-face {
+  font-family: "Futura";
+  src: local("Futura"),
+    url(~assets/futura-pt-heavy.otf) format("truetype");
+}
+/*@font-face {
   font-family: "Futura Bold";
   src: url("Futura Bold");
   font-weight: bold;
-}
+}*/
 .swal2-footer{
   align-items: center;
 }
