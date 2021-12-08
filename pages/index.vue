a407c9e7b051e9d89e445bcfc6f6de61c2c54abb
Karl <local-part@domain>
2021-12-08 03:18:59 +0100

    [/pages/] index.vue updated.

--- pages/index.vue
+++ pages/index.vue
@@ -192,8 +192,8 @@ export default {
     EndGame(){
       Swal.fire({
         title: `You scored ${this.score}, ${this.perfects} perfects and a max strike of ${this.maxPerfects}.\n You are ${this.percentage}% as skilled as the king!`,
-        background: `#fff url("${require('/assets/img/most-beautiful-background-picture.jpg')}")`,
-        footer: `<p>Share on social media: </p><img src="${require('/assets/img/Twitter.svg')}" onclick="click_social_media(\'twitter-d\')"><img src="${require('../assets/img/TikTok.svg')}" onclick="click_social_media(\'facebook-d\')"> or download : <img src="${require('../assets/img/download.svg')}" onclick="downloadscreenshot()">`,
+        background: `#E6ABBD url("${require('/assets/img/main-cover.jpeg')}")`,
+        footer: `<p>Share on social media: </p><img src="${require('/assets/img/twitter.svg')}" onclick="click_social_media(\'twitter-d\')"><img src="${require('../assets/img/tiktok.svg')}" onclick="click_social_media(\'facebook-d\')"> or download : <img src="${require('../assets/img/download.svg')}" onclick="downloadscreenshot()">`,
         imageUrl: require('/assets/img/FEqSaeQWYAcu0ln.jpeg'),
         showConfirmButton: false,
         color: "white",
